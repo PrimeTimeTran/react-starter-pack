07e824073f80e8bcded2b1661d445b5ce252f704
Loi Van Tran <datloiboi@gmail.com>
2021-01-12 09:43:53 +0700

    Remove _redirects and CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -91,16 +91,6 @@ module.exports = (env) => {
       filename: 'bundle.js',
     },
     plugins: [
-      // new CopyPlugin([
-      //   {
-      //     from: path.resolve(__dirname, '../static'),
-      //     to: config.build.assetsSubDirectory,
-      //     ignore: ['.*'],
-      //   },
-      // ]),
-      new CopyPlugin({
-        patterns: [{ from: '_redirects', to: 'public/_redirects' }],
-      }),
       new HtmlWebpackPlugin({
         template: path.join(__dirname, 'public', 'index.html'),
       }),
