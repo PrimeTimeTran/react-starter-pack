3f6e26527cbd3155a15cc46faf39c11a5c7554fe
Loi Van Tran <datloiboi@gmail.com>
2021-01-14 12:31:27 +0700

    Update TodosPage

--- src/pages/TodosPage/TodosPage.component.tsx
+++ src/pages/TodosPage/TodosPage.component.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from 'react';
+import React, { useState, Component, ReactNode } from 'react';
 import { IProps } from './TodosPage';
 import { Grid, TextField, Select, MenuItem, Button } from '@material-ui/core';
 import { Buttons } from 'components/Buttons';
@@ -6,46 +6,40 @@ import { i18n } from 'utils/lib/i18n';
 import { MainWrapper } from 'components/MainWrapper';
 import cra from 'assets/images/cra.png';
 
+type Todo = {
+  text: string;
+  complete: boolean;
+}
+
+interface TodoListItemProps {
+  todo: Todo
+}
+
+const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+  return (
+    <div style={{ flexDirection: 'row' }}>
+      <input type="checkbox" checked={todo.complete} />
+      <input type="checkbox" checked={true} />
+      <input type="checkbox" checked={false} />
+      <strong>{todo.text}</strong>
+    </div>
+  )
+}
+
+const todos: Array<Todo> = [
+  { text: 'Walk Dog', complete: true },
+  { text: 'Code an app', complete: false },
+  { text: 'Learn TypeScript', complete: false },
+]
+
 export class TodosPage extends Component<IProps> {
   public render(): ReactNode {
-    const changeLanguages = (lng: string) => {
-      i18n.changeLanguage(lng);
-      return window.location.reload(false);
-    };
     return (
       <MainWrapper>
         <div className="home">
-          <img
-            className="logo"
-            src={cra}
-            alt="ToolsForProductivity"
-          />
-          <p className="text-center">{i18n.t('general.description')}</p>
           <Grid container spacing={3}>
             <Grid item xs={12} className="text-center">
-              <Buttons
-                color="primary"
-                text={i18n.t('general.language.english')}
-                onClick={() => changeLanguages('en')}
-              />
-              <Buttons
-                color="primary"
-                text={i18n.t('general.language.vietnam')}
-                onClick={() => changeLanguages('vn')}
-              />
-              <form noValidate autoComplete="off" onSubmit={(e) => {
-                e.preventDefault()
-                console.log('Gogogogogo')
-              }}>
-                <TextField id="standard-basic" label="Standard" />
-                <Select labelId="label" id="select" value="20">
-                  <MenuItem value="10">Ten</MenuItem>
-                  <MenuItem value="20">Twenty</MenuItem>
-                </Select>
-
-                <Button variant="contained" type="submit">Submit</Button>
-
-              </form>
+              <TodoListItem todo={{ text: 'gogogo', complete: true }}></TodoListItem>
             </Grid>
           </Grid>
         </div>
